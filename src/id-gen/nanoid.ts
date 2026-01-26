e5ed915c3c94fb8b04b23cd4cede799bb06e1cc0
Mohtasim Alam <94981761+shimizudev@users.noreply.github.com>
2026-01-26 22:34:19 +0600

    Add keywords nano id

--- src/id-gen/nanoid.ts
+++ src/id-gen/nanoid.ts
@@ -2,3 +2,4 @@ import { customAlphabet } from 'nanoid'
 
 const alphabet = 'abcdefghijklmnopqrstuvwxyz0123456789_'
 export const nanoid = customAlphabet(alphabet, 16)
+export const keywordsNanoId = customAlphabet(alphabet, 6);
