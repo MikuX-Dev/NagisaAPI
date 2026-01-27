238ab1e3de7ab459162d441761a8866ea90ea2d1
Mohtasim Alam <94981761+shimizudev@users.noreply.github.com>
2026-01-27 16:12:55 +0600

    add kitsu and ex server

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,9 @@
-console.log('Hello via Bun!')
+import cors from '@elysiajs/cors'
+import Elysia from 'elysia'
+
+new Elysia()
+  .use(cors())
+  .get('/', () => ({
+    message: 'Elo! Fuck you<3',
+  }))
+  .listen(3000)
