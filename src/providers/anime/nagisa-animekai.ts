97d11cee536c815222294652087be89bbacc9814
Mohtasim Alam <94981761+shimizudev@users.noreply.github.com>
2026-01-30 00:33:05 +0600

    plat is the best namer ngl

---
+++ src/providers/anime/nagisa-animekai.ts
@@ -0,0 +1,95 @@
+import { Anime } from '@crysoline/lib'
+import type {
+  CrysolineProviderEpisode,
+  ProviderInfo,
+  ProviderSearch,
+} from '../../types/provider'
+
+import { AnimeBase } from '../base/anime'
+
+class Nagisa extends AnimeBase {
+  override name: string = 'nagisa'
+  override url = 'https://anikai.to'
+  override providerType: ('H-SUB' | 'SUB' | 'DUB')[] = ['H-SUB', 'DUB']
+
+  private provider = Anime.AnimeKai(this.apiKey)
+
+  override async search(query: string): Promise<ProviderSearch[] | undefined> {
+    try {
+      const res = await this.provider.search(query)
+
+      return res.map((r) => ({
+        title:
+          r.title?.english ??
+          r.title?.romaji ??
+          r.title?.japanese ??
+          r.title?.japanese ??
+          r.title?.other ??
+          '',
+        id: r.id ?? '',
+        year: (r as { year: number })?.year ?? undefined,
+        totalEpisodes: r.totalEpisodes ?? undefined,
+      }))
+    } catch {
+      return undefined
+    }
+  }
+
+  override async getInfo(id: string): Promise<ProviderInfo | undefined> {
+    try {
+      const res = await this.provider.info(id)
+
+      const info: ProviderInfo = {
+        id: res.id?.toString() ?? undefined,
+        titles: [
+          {
+            languageCode: 'english',
+            title: res.title?.english ?? '',
+          },
+          {
+            languageCode: 'romaji',
+            title: res.title?.romaji ?? '',
+          },
+          {
+            languageCode: 'japanese',
+            title: res.title?.japanese ?? '',
+          },
+        ],
+
+        createdAt: Date.now(),
+        updatedAt: Date.now(),
+      }
+
+      return info
+    } catch {
+      return undefined
+    }
+  }
+
+  override async getEpisodes(
+    id: string,
+  ): Promise<CrysolineProviderEpisode[] | undefined> {
+    try {
+      const res = await this.provider.episodes(id)
+      const episodes: CrysolineProviderEpisode[] = res.map((r) => ({
+        titles: [
+          {
+            languageCode: 'english',
+            title: r.title ?? '',
+          },
+        ],
+        id: r.id?.toString(),
+        hasDub: r.metadata?.dub,
+
+        createdAt: Date.now(),
+        updatedAt: Date.now(),
+      }))
+
+      return episodes
+    } catch {
+      return undefined
+    }
+  }
+}
+
+export default Nagisa
